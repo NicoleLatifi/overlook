f68318653c15ddbdd494b672ff519e1a5e7dfd3c
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-30 13:12:24 -0600

    Add beforeEach setup #16

--- test/Customers-test.js
+++ test/Customers-test.js
@@ -2,7 +2,17 @@ import chai from 'chai';
 const expect = chai.expect;
 
 describe('Customers', function() {
-  it('', function() {
+  let customers = [];
+  let customer1;
+  let customer2;
+  let customer3;
+  beforeEach(() => {
+    customer1 = {"id": 1, "name": "Leatha Ullrich"};
+    customer2 = {"id": 2, "name": "Rocio Schuster"};
+    customer3 = {"id": 3, "name": "Kelvin Schiller"};
+    customers.push(customer1, customer2, customer3)
+  }
+  it('Should hold all customers', function() {
 
     expect().to.equal();
   });

6258b3d83fd693364a4f77847427395f5386b232
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-30 14:32:39 -0600

    Add method that passes test to get id by name #16

--- src/AllCustomers.js
+++ src/AllCustomers.js
@@ -1,5 +1,16 @@
 const AllCustomers = {
-  customers: []
+  customers: [],
+
+  getCustomerIdByName(name) {
+    //starting with array of objects with properties id and name
+    //end result is one name
+    //iterate over customers with find looking for where name === customer.name
+    //this will return the full customer object
+    //add .id to the end to get just the id
+    return this.customers.find(customer => {
+      return name === customer.name
+    }).id
+  }
 }
 
 export default AllCustomers;

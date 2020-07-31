717e23720cb9c7d77d0f3641e4db197f47ae91e1
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-30 19:57:51 -0600

    Write test for get id by partial name #16

--- test/AllCustomers-test.js
+++ test/AllCustomers-test.js
@@ -6,15 +6,16 @@ describe('AllCustomers', function() {
   let customer1;
   let customer2;
   let customer3;
+  let allCustomers;
   beforeEach(() => {
     customer1 = {"id": 1, "name": "Leatha Ullrich"};
     customer2 = {"id": 2, "name": "Rocio Schuster"};
     customer3 = {"id": 3, "name": "Kelvin Schiller"};
-    AllCustomers.customers.push(customer1, customer2, customer3)
+    allCustomers = new AllCustomers([customer1, customer2, customer3])
   });
 
   it('Should hold all customers', function() {
-    expect(AllCustomers.customers).to.deep.equal([
+    expect(allCustomers.customers).to.deep.equal([
       {"id": 1, "name": "Leatha Ullrich"},
       {"id": 2, "name": "Rocio Schuster"},
       {"id": 3, "name": "Kelvin Schiller"}
@@ -22,8 +23,14 @@ describe('AllCustomers', function() {
   });
 
   it('Should get customer Id by name', function() {
-    const customerId = AllCustomers.getCustomerIdByName("Leatha Ullrich")
+    const customerId = allCustomers.getCustomerIdByName("Leatha Ullrich");
+
+    expect(customerId).to.equal(1);
+  });
+
+  it('Should get customer Id given partial name', function() {
+    const customerId = allCustomers.getCustomerIdByName("Leatha");
 
     expect(customerId).to.equal(1);
   })
-});
+}); 

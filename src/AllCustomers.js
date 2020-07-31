2347dc82bbbb715528218cc4ef4d6a64f491a1a0
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-30 20:09:19 -0600

    Refactor so that if no customer is found a relative message is returned #16

--- src/AllCustomers.js
+++ src/AllCustomers.js
@@ -4,15 +4,14 @@ class AllCustomers {
   }
     
   getCustomerIdByName(name) {
-    //starting with array of objects with properties id and name
-    //end result is one name
-    //iterate over customers with find looking for where name === customer.name
-    //this will return the full customer object
-    //add .id to the end to get just the id
-    return this.customers.find(customer => {
+    const foundCustomer = this.customers.find(customer => {
       return customer.name.includes(name)
-      // return name === customer.name
-    }).id
+    })
+    if (foundCustomer === undefined) {
+      return `No customer found with the name ${name}. Please try again.`
+    } else {
+      return foundCustomer.id
+    }
   }
 }
 

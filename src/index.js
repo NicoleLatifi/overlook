62f5a898815ddd52845b7946ff49f8bb5bc8af7f
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-01 14:22:50 -0600

    Write first version of determineUser function that logs the username or an alert message #27

--- src/index.js
+++ src/index.js
@@ -9,10 +9,44 @@ import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
+const body = document.querySelector("body");
+const usernameInput = document.querySelector(".username-input");
+const passwordInput = document.querySelector(".password-input");
+
+let customer;
+let manager;
+
 body.addEventListener("click", clickHandler);
 
 function clickHandler() {
   if (event.target.classList.contains("submit")) {
+    event.preventDefault();
     determineUser();
   }
+}
+
+//maybe break into determineValidInput and determineUser functions
+function determineUser() {
+  let usernameInputRoot = usernameInput.value.slice(0, 8);
+  let usernameInputId = usernameInput.value.slice(8);
+  let inputId = parseInt(usernameInputId);
+  if (usernameInputRoot === "customer" && inputId >= 1 && inputId <= 50) {
+    loginCustomer(usernameInputRoot);
+  } else if (usernameInputRoot === "manager") {
+    loginManager(name);
+  } else {
+    alertMessage("invalid username");
+  }
+}
+
+function alertMessage(message) {
+  console.log(message)
+}
+
+function loginCustomer(name) {
+  console.log(name)
+}
+
+function loginManager(name) {
+  console.log(name)
 }

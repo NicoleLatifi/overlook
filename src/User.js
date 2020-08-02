2e2151a3d1bdecc1441d7c670ff1ec7724886814
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-01 21:21:50 -0600

    Remove getName functionality that was moved to index.js #27

--- src/User.js
+++ src/User.js
@@ -8,23 +8,7 @@ class User {
   };
 
   getName() {
-    let id;
-    let userFound;
-    let usernameRoot = this.username.slice(0, 8);
-    let index8Code = this.username.charCodeAt(8);
-    // let index9Code = this.username.charCodeAt(9);
-    if (usernameRoot === "customer" && index8Code >= 48 && index8Code <= 57) {
-      id = this.username.slice(8);
-      userFound = this.allCustomers.customers.find(customer => {
-        return customer.id == id
-        }).name;
-    } else if (usernameRoot === "manager") {
-      userFound = "manager"
-    } else {
-      return "invalid username"
-    }
-   return userFound;
-  }
+   
 }
 
 export default User;

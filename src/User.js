52605cbbae3729921a8d9646f85da53b8b6a9dcc
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-04 12:54:00 -0600

    Try addBooking method in different classes #46

--- src/User.js
+++ src/User.js
@@ -6,6 +6,20 @@ class User {
     // this.allCustomers = allCustomers;
     this.username = username;
   }
+
+  //This works in customer, can it go here?
+  // addBooking() {
+  //   const postBookingData = {"userID": 23, "date": "2020/05/13", "roomNumber": 5};
+  //   fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings", {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(postBookingData),
+  //   })
+  //     .then(response => response.json())
+  //     .catch(error => console.log(error));
+  // }
 }
 
 export default User;
